e1fcd80c1aeec7b8098b6df8c16770d033fb22f0
Jamieie <116743317+Jamieie@users.noreply.github.com>
2025-08-21 16:49:01 +0900

    fix: AuthServiceImpl 클래스에 Transactional 추가

--- src/main/java/org/example/todoapp/service/AuthServiceImpl.java
+++ src/main/java/org/example/todoapp/service/AuthServiceImpl.java
@@ -11,8 +11,10 @@ import org.example.todoapp.mappers.UsersMapper;
 import org.example.todoapp.util.DtoVoConverter;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
